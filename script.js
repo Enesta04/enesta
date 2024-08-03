7203287686b7c3eb0b0413862d7ecf1be85b5fb8
Enesta04 <135735973+Enesta04@users.noreply.github.com>
2024-08-03 12:45:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,78 @@ document.querySelector(".left-soft-title").addEventListener("click", function ()
 		boxSoftText.style.color = "#2B2B2B";
 	}
 });
+
+let boxStandart = document.querySelector(".left-STANDART-skills");
+let boxStandartIcon = document.querySelector(".left-STANDART-title-icon");
+let boxStandartText = document.querySelector(".left-STANDART-title-text");
+document.querySelector(".left-STANDART-title").addEventListener("click", function (){
+	if (boxStandart.style.display === "flex"){
+		boxStandart.style.display = "none";
+		boxStandartIcon.src = "assests/Arrow1.svg";
+		boxStandartText.style.color = "#767676";
+	} else {
+		boxStandart.style.display = "flex";
+		boxStandartIcon.src = "assests/Arrow2.svg";
+		boxStandartText.style.color = "#2B2B2B";
+	}
+});
+
+let boxVip = document.querySelector(".left-VIP-skills");
+let boxVipIcon = document.querySelector(".left-VIP-title-icon");
+let boxVipText = document.querySelector(".left-VIP-title-text");
+document.querySelector(".left-VIP-title").addEventListener("click", function (){
+	if (boxVip.style.display === "flex"){
+		boxVip.style.display = "none";
+		boxVipIcon.src = "assests/Arrow1.svg";
+		boxVipText.style.color = "#767676";
+	} else {
+		boxVip.style.display = "flex";
+		boxVipIcon.src = "assests/Arrow2.svg";
+		boxVipText.style.color = "#2B2B2B";
+	}
+});
+
+let boxLight = document.querySelector(".left-LIGHT-skills");
+let boxLightIcon = document.querySelector(".left-LIGHT-title-icon");
+let boxLightText = document.querySelector(".left-LIGHT-title-text");
+document.querySelector(".left-LIGHT-title").addEventListener("click", function (){
+	if (boxLight.style.display === "flex"){
+		boxLight.style.display = "none";
+		boxLightIcon.src = "assests/Arrow1.svg";
+		boxLightText.style.color = "#767676";
+	} else {
+		boxLight.style.display = "flex";
+		boxLightIcon.src = "assests/Arrow2.svg";
+		boxLightText.style.color = "#2B2B2B";
+	}
+});
+
+let boxSmm = document.querySelector(".left-SMM-skills");
+let boxSmmIcon = document.querySelector(".left-SMM-title-icon");
+let boxSmmText = document.querySelector(".left-SMM-title-text");
+document.querySelector(".left-SMM-title").addEventListener("click", function (){
+	if (boxSmm.style.display === "flex"){
+		boxSmm.style.display = "none";
+		boxSmmIcon.src = "assests/Arrow1.svg";
+		boxSmmText.style.color = "#767676";
+	} else {
+		boxSmm.style.display = "flex";
+		boxSmmIcon.src = "assests/Arrow2.svg";
+		boxSmmText.style.color = "#2B2B2B";
+	}
+});
+
+let boxCon = document.querySelector(".left-CON-skills");
+let boxConIcon = document.querySelector(".left-CON-title-icon");
+let boxConText = document.querySelector(".left-CON-title-text");
+document.querySelector(".left-CON-title").addEventListener("click", function (){
+	if (boxCon.style.display === "flex"){
+		boxCon.style.display = "none";
+		boxConIcon.src = "assests/Arrow1.svg";
+		boxConText.style.color = "#767676";
+	} else {
+		boxCon.style.display = "flex";
+		boxConIcon.src = "assests/Arrow2.svg";
+		boxConText.style.color = "#2B2B2B";
+	}
+});
